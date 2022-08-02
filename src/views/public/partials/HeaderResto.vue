bf699ef90214d6667faf3153b30edc0e2e66e23f
unknown <davidpouani228@gmail.com>
2022-08-02 04:39:53 +0100

    create page restaurant kis

--- src/views/public/partials/HeaderResto.vue
+++ src/views/public/partials/HeaderResto.vue
@@ -1,16 +1,22 @@
 <template lang="">
     <div>
-        <div class="header_resto">
+        <div class="header_resto py-5 py-md-0">
             <img src="@/assets/nyama_image/slide-01.webp" alt="" />
             <div class="header_resto-text">
-                Parcourer la lits de vos restaurants
+                <h2>Parcourer la lits de vos restaurants</h2>
+                <Multiselect class="col-md" v-model="value"
+                    :options="options" :custom-label="value"
+                    placeholder="Select one" label=""></Multiselect>
             </div>
         </div>
     </div>
 </template>
 <script>
+import Multiselect from '@vueform/multiselect'
 export default {
-    
+    components:{
+        Multiselect,
+    },
 }
 </script>
 <style scoped>
@@ -24,8 +30,22 @@ export default {
     .header_resto-text{
         z-index: 2;
         position: absolute;
-        top: 30%;
+        top: 35%;
         left: 12%;
         right: 0;
+        width: 60%;
+    }
+    @media screen and (min-width: 760px) {
+        .header_resto-text{
+            z-index: 2;
+            position: absolute;
+            top: 35%;
+            left: 12%;
+            right: 0;
+            width: 30%;
+        }
+        .header_resto h2{
+            font-size: 3rem;
+        }
     }
 </style>
